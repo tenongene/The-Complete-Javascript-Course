e546ae924f7047c8711da9a00f3f8c02221014f0
Thierry Enongene <thierryss@gmail.com>
2021-11-21 02:07:17 -0600

    Part 9 cod chall 1

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -27,3 +27,81 @@ const restaurant = {
     },
   },
 };
+
+const game = {
+  team1: 'Bayern Munich',
+  team2: 'Borussia Dortmund',
+  players: [
+    [
+      'Neuer',
+      'Pavard',
+      'Martinez',
+      'Alaba',
+      'Davies',
+      'Kimmich',
+      'Goretzka',
+      'Coman',
+      'Muller',
+      'Gnarby',
+      'Lewandowski',
+    ],
+    [
+      'Burki',
+      'Schulz',
+      'Hummels',
+      'Akanji',
+      'Hakimi',
+      'Weigl',
+      'Witsel',
+      'Hazard',
+      'Brandt',
+      'Sancho',
+      'Gotze',
+    ],
+  ],
+  score: '4:0',
+  scored: ['Lewandowski', 'Gnarby', 'Lewandowski', 'Hummels'],
+  date: 'Nov 9th, 2037',
+  odds: { team1: 1.33, x: 3.25, team2: 6.5 },
+};
+//1.
+// const players1 = game.players[0];
+// const players2 = game.players[1];
+const [players1, players2] = game.players;
+
+//2.
+const [gk, ...fieldPlayers] = players2;
+console.log(gk);
+console.log(fieldPlayers);
+
+//3.
+const allPlayers = [...players1, ...players2];
+console.log(allPlayers);
+
+//4.
+// let players1Final = game.players[0].push('Thiago', 'Coutinho', 'Perisic');
+// players1Final = game.players[0];
+const players1Final = [...players1, 'Thiago', 'Coutinho', 'Perisic'];
+console.log(players1Final);
+
+//5.
+// const team1 = game.odds.team1;
+// const draw = game.odds.x;
+// const team2 = game.odds.team2;
+const {odds: {team1, x, team2}} = game;
+console.log(team1, x, team2);
+
+//6.
+const printGoals = function (...player) {
+  return console.log(`${player.length} goals scored.`);
+};
+
+
+//7. 
+team1 < team2 && console.log("Team 1 is more likely to win");
+
+
+
+
+
+printGoals('Lewandowski', 'Hummels');
--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -1,1 +1,40 @@
 'use strict';
+
+const lufthansa = {
+  airline: 'lufthansa',
+  iataCode: 'LH',
+  bookings: [],
+  book(flightNum, name) {
+    console.log(
+      `${name} booked a seat on ${this.airline} flight ${this.iataCode}${flightNum}`
+    );
+  },
+};
+
+// lufthansa.book(277, "Bryson Enongene");
+
+const poll = {
+  question: 'What is your favourite programming language?',
+  options: ['0: JavaScript', '1: Python', '2: Rust', '3: C++'],
+  answers: new Array(4).fill(0),
+  registerNewAnswer: function () {
+    let answer = prompt(`${this.question}: 
+        ${this.options} (Write option number)`);
+    if (Number(answer) <= 3) {
+      this.answers.splice(Number(answer), 1, 1);
+      return this.answers;
+    } else {
+      console.log('Not a valid input');
+    }
+  },
+};
+
+//const newAnswer = poll.registerNewAnswer();
+const pollBtn = document.querySelector('.poll');
+pollBtn.addEventListener('click', poll.registerNewAnswer.bind(poll));
+
+//console.log(poll.answers);
+// console.log(poll.answers.splice(2,1,1));
+// console.log(poll.answers);3
+// console.log(poll.answers.splice(2,1,+1))
+// console.log(poll.answers);
